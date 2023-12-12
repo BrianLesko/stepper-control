dea99553a4ffd9d015b930f673c33aa49d9c9798
Brian Lesko <brianjosephlesko@gmail.com>
2023-12-12 00:51:12 -0500

    Added Fullstep, single phase step, and half stepping
    Fixed a mistake where I indicated a single phase step code was fulls step, and actually implemented full stepping correctly as well as half stepping

--- stepper_forward/stepper_forward.ino
+++ stepper_forward/stepper_forward.ino
@@ -11,11 +11,15 @@ void setup() {
 }
 
 void loop(){
-  fullStep(delayLength, false); // Forward step
+  halfStep(delayLength, false); // Forward step
 }
 
-void fullStep(int delayLength, bool backwards) {
+void singlePhaseStep(int delayLength, bool backwards) {
+  // Single phase stepping is activating one coil at a time
+  // not as strong as full step
+  // A+ B+ A- B-
 
+  // A 
   digitalWrite(9, LOW);  //ENABLE CH A
   digitalWrite(8, HIGH); //DISABLE CH B
   digitalWrite(12, HIGH);   //Sets direction of CH A
@@ -23,6 +27,7 @@ void fullStep(int delayLength, bool backwards) {
   
   delay(delayLength);
   
+  // B 
   digitalWrite(9, HIGH);  //DISABLE CH A
   digitalWrite(8, LOW); //ENABLE CH B
   digitalWrite(13, HIGH);   //Sets direction of CH B
@@ -31,13 +36,15 @@ void fullStep(int delayLength, bool backwards) {
   
   delay(delayLength);
   
+  // A-
   digitalWrite(9, LOW);  //ENABLE CH A
   digitalWrite(8, HIGH); //DISABLE CH B
   digitalWrite(12, LOW);   //Sets direction of CH A
   analogWrite(3, 255);   //Moves CH A
   
   delay(delayLength);
-    
+  
+  // B-
   digitalWrite(9, HIGH);  //DISABLE CH A
   digitalWrite(8, LOW); //ENABLE CH B
   digitalWrite(13, LOW);   //Sets direction of CH B
@@ -46,3 +53,144 @@ void fullStep(int delayLength, bool backwards) {
   
   delay(delayLength);
 }
+
+void fullStep(int delayLength, bool backwards) {
+  // full step is much stronger than single phase step
+  // A+B+ > A-B+ > A-B- > A+B-
+  digitalWrite(9, LOW);  //ENABLE CH A
+  digitalWrite(8, LOW); //ENABLE CH B
+
+  // AB
+  digitalWrite(12, HIGH);   //Sets direction of CH A as +
+  digitalWrite(13, HIGH);   //Sets direction of CH B as + 
+  analogWrite(3, 255);   //Moves CH A
+  analogWrite(11, 255);   //Moves CH B
+
+  delay(delayLength);
+
+  //A-B
+  digitalWrite(12, LOW);   //Sets direction of CH A as -
+  digitalWrite(13, HIGH);   //Sets direction of CH B as + 
+  analogWrite(3, 255);   //Moves CH A
+  analogWrite(11, 255);   //Moves CH B
+
+  delay(delayLength);
+
+  //A-B-
+  digitalWrite(12, LOW);   //Sets direction of CH A as -
+  digitalWrite(13, LOW);   //Sets direction of CH B as -
+  analogWrite(3, 255);   //Moves CH A
+  analogWrite(11, 255);   //Moves CH B
+
+  delay(delayLength);
+
+  // AB-
+  digitalWrite(12, HIGH);   //Sets direction of CH A as +
+  digitalWrite(13, LOW);   //Sets direction of CH B as -
+  analogWrite(3, 255);   //Moves CH A
+  analogWrite(11, 255);   //Moves CH B
+
+  delay(delayLength);
+
+  // A-
+  digitalWrite(9, HIGH);  //DISABLE CH A
+  digitalWrite(8, LOW); //ENABLE CH B
+  digitalWrite(13, LOW);   //Sets direction of CH B
+  if (backwards){digitalWrite(13, HIGH);}
+  analogWrite(11, 255);   //Moves CH B
+  
+}
+
+void halfStep(int delayLength, bool backwards) {
+  // AB > B > A-B+ > A- > A-B- > B- > A+B- > A
+  // half stepping has maximum torque on double activations but less torque on single activation steps, it is much smoother and quieter
+  // Source: https://www.monolithicpower.com/bipolar-stepper-motors-part-i-control-modes#:~:text=In%20half%2Dstep%20mode%2C%20both,phase%20to%20two%2Dphase%20operation.
+
+  delayLength = delayLength/2;
+
+  digitalWrite(9, LOW);  //ENABLE CH A
+  digitalWrite(8, LOW); //ENABLE CH B
+
+  // AB
+  digitalWrite(12, HIGH);   //Sets direction of CH A as +
+  digitalWrite(13, HIGH);   //Sets direction of CH B as + 
+  analogWrite(3, 255);   //Moves CH A
+  analogWrite(11, 255);   //Moves CH B
+
+  delay(delayLength);
+
+  // B 
+  digitalWrite(9, HIGH);  //DISABLE CH A
+  digitalWrite(8, LOW); //ENABLE CH B
+  digitalWrite(13, HIGH);   //Sets direction of CH B
+  if (backwards){digitalWrite(13, LOW);}
+  analogWrite(11, 255);   //Moves CH B
+
+  delay(delayLength);
+
+  //A-B
+  digitalWrite(9, LOW);  //ENABLE CH A
+  digitalWrite(12, LOW);   //Sets direction of CH A as -
+  digitalWrite(13, HIGH);   //Sets direction of CH B as + 
+  analogWrite(3, 255);   //Moves CH A
+  analogWrite(11, 255);   //Moves CH B
+
+  delay(delayLength);
+
+  // A-
+  digitalWrite(9, LOW);  //ENABLE CH A
+  digitalWrite(8, HIGH); //DISABLE CH B
+  digitalWrite(12, LOW);   //Sets direction of CH A
+  analogWrite(3, 255);   //Moves CH A
+
+  delay(delayLength);
+
+  //A-B-
+  digitalWrite(8, LOW);  //ENABLE CH B
+  digitalWrite(12, LOW);   //Sets direction of CH A as -
+  digitalWrite(13, LOW);   //Sets direction of CH B as -
+  analogWrite(3, 255);   //Moves CH A
+  analogWrite(11, 255);   //Moves CH B
+
+  delay(delayLength);
+
+  // B-
+  digitalWrite(9, HIGH);  //DISABLE CH A
+  digitalWrite(8, LOW); //ENABLE CH B
+  digitalWrite(13, LOW);   //Sets direction of CH B
+  if (backwards){digitalWrite(13, HIGH);}
+  analogWrite(11, 255);   //Moves CH B
+
+  delay(delayLength);
+
+  // AB-
+  digitalWrite(9, LOW);  //ENABLE CH A  
+  digitalWrite(12, HIGH);   //Sets direction of CH A as +
+  digitalWrite(13, LOW);   //Sets direction of CH B as -
+  analogWrite(3, 255);   //Moves CH A
+  analogWrite(11, 255);   //Moves CH B
+
+  delay(delayLength);
+
+  // A 
+  digitalWrite(9, LOW);  //ENABLE CH A
+  digitalWrite(8, HIGH); //DISABLE CH B
+  digitalWrite(12, HIGH);   //Sets direction of CH A
+  analogWrite(3, 255);   //Moves CH A
+
+  delay(delayLength);
+}
+
+void quarterStep(){
+  // Quarter step sequence
+  // 1. A
+  // 2. AB with B at half current
+  // 3. B
+  // 4. -A B with A at half current
+  // 5. -A
+  // 6. -A -B with B at half current
+  // 7. -B
+  // 8. A -B with A at half current
+  // Repeat sequence
+}
+
